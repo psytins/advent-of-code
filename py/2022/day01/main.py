b05fc89cdd60e0f2a6f3709dfc0666693af9194f
gMartins <45578391+psytins@users.noreply.github.com>
2022-12-02 23:27:06 +0000

    day 01 completed

--- py/2022/day01/main.py
+++ py/2022/day01/main.py
@@ -1,20 +1,38 @@
 # Link to Day 01 - https://adventofcode.com/2022/day/1
 import sys
-
 input = sys.argv[1] # get the input file
 
-sum_of_calories = 0
-higher_sum_of_calories = 0
+## PART ONE -----------
+def part01():
+    sum_of_calories = 0
+    higher_sum_of_calories = 0
 
-for x in open(input).read().split("\n"):
-    if x.strip():
-        n = int(x)
-        sum_of_calories = sum_of_calories + n
+    for x in open(input).read().split("\n"):
+        if x.strip():
+            n = int(x)
+            sum_of_calories = sum_of_calories + n
 
-    if x == "":
-        if sum_of_calories > higher_sum_of_calories:
-            higher_sum_of_calories = sum_of_calories
-        
-        sum_of_calories = 0
+        if x == "":
+            if sum_of_calories > higher_sum_of_calories:
+                higher_sum_of_calories = sum_of_calories
             
-print(higher_sum_of_calories)
+            sum_of_calories = 0
+                
+    print(higher_sum_of_calories)
+
+## PART TWO -----------
+def part02():
+    sum_of_calories = 0
+    totalList = list()
+
+    for x in open(input).read().split("\n"):
+        if x.strip():
+            n = int(x)
+            sum_of_calories = sum_of_calories + n
+
+        if x == "":
+            totalList.append(sum_of_calories)
+            sum_of_calories = 0
+
+    totalList.sort(reverse=True)  
+    print(sum(totalList[0:3]))
